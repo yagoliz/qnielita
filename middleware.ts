38b535e082dd67d5a26a3a61b4252a5040453362
Yago Lizarribar <yago.lizarribar@gmail.com>
2026-05-27 17:30:36 +0200

    add cron to skipped middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|flags/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api/cron/|_next/static|_next/image|favicon.ico|flags/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
